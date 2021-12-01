f4ed0556066af0b9d3984e5024f9ab9450e6d276
mojahige <mojahige@gmail.com>
2021-12-01 22:10:06 +0900

    add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      extends: ["eslint:recommended", "prettier"],
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+      ],
+    },
+  ],
+};
